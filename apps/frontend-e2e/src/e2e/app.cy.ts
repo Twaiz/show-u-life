6ba70f5e68a7b2b9445deaee2e1f5becb72ffbea
Twaiz <homenkobodya@gmail.com>
2025-03-19 22:58:10 +0200

    fix[apps/frontend]: Fixed a bug with starting the frontend server

--- apps/frontend-e2e/src/e2e/app.cy.ts
+++ apps/frontend-e2e/src/e2e/app.cy.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('frontend-e2e', () => {
+describe('@show-u-life/frontend-e2e', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
